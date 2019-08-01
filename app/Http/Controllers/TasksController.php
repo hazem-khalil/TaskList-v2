d10ba9404542bde0c8102b6520626d71cd53de16
Hazem Khalil <hazem.ibrahim.khalil@gmail.com>
2019-08-02 01:02:06 +0200

    Creating tasks

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Task;
 
 class TasksController extends Controller
 {
@@ -17,13 +18,18 @@ class TasksController extends Controller
         return view('tasks.index');
     }
 
-    public function store()
+    public function store(Request $request)
     {
         $this->validate(request(), [
             'name' => 'required|max:255',
         ]);
 
-        
+
+        $request->user()->tasks()->create([
+            'name' => request('name')
+        ]);
+
+        return redirect()->back();        
     }
 
     public function destroy()
--- app/Task.php
+++ app/Task.php
@@ -3,8 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Task;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function publish($task)
+    {
+        Task::create([
+            'name' => $task,
+            'user_id' => auth()->id()
+        ]);
+    }  
 }
